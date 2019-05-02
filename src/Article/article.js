edded432cc1468fbdc60477dccb48d8a0fd25605
SERENDIPITY96 <justserendipity96@gmail.com>
2019-05-02 12:43:03 +0800

    动态获取article

--- src/Article/article.js
+++ src/Article/article.js
@@ -1,51 +1,78 @@
 import React from 'react'
 import './article.less'
-class Article extends React.Component{
+
+const tagsUrl = 'http://localhost:8081/tags'
+const articleUrl = 'http://localhost:8081/articleList'
+
+class Article extends React.Component {
+
+    state = {
+        tagsList: [],
+        currentIndex: 0,
+        articleList: []
+    }
+
+    componentDidMount() {
+        this.getTagsList()
+        this.getArticleList(0)
+    }
+
+
+    getTagsList() {
+        const _this = this
+        fetch(tagsUrl, {
+            method: 'GET'
+        }).then((res) => res.json())
+            .then((res) => {
+                _this.setState({tagsList: res})
+                console.log(res)
+            })
+    }
+
+    changeTag(index) {
+        this.setState({currentIndex: index})
+        this.getArticleList(index)
+    }
+
+    getArticleList(index) {
+        const _this = this
+        fetch(articleUrl, {
+            method: 'POST',
+            body: index
+        }).then((res) => res.json())
+            .then(res => {
+                _this.setState({articleList:res})
+                console.log(res)
+            })
+    }
 
     render() {
-        return(
+        const {tagsList, currentIndex,articleList} = this.state
+        console.log(articleList)
+        return (
             <div className='content'>
                 <div className='category'>
                     <ul>
-                        <li><a href='./'>前端</a></li>
-                        <li><a href='./'>Rust</a></li>
-                        <li><a href='./'>算法</a></li>
+                        {tagsList.map((item, index) => (
+                            <li key={index} className={index === currentIndex ? 'tagItem_active' : ''}
+                                onClick={this.changeTag.bind(this, index)}><span>{item.tag_name}</span></li>
+                        ))}
                     </ul>
                 </div>
                 <ul className='article-list'>
-                    <li className='article-item'>
-                        <div>
-                        <img src={require('../img/article-img.jpg')} alt='img'/>
-                        </div>
-                        <div className='word'>
-                            <h2>空间立体效果图,完美呈现最终效果</h2>
-                            <h5>设计文章</h5>
-                            <p>室内设计作为一门新兴的学科，尽管还只是近数十年的事，但是人们有意识地对自己生活、生产活动的室内进行安排布置，甚至美化装饰，赋予室内环境</p>
-                            <a href='./'>阅读更多</a>
-                        </div>
-                    </li>
-                    <li className='article-item'>
-                        <div>
-                            <img src={require('../img/article-img.jpg')} alt='img'/>
-                        </div>
-                        <div className='word'>
-                            <h2>空间立体效果图,完美呈现最终效果</h2>
-                            <h5>设计文章</h5>
-                            <p>室内设计作为一门新兴的学科，尽管还只是近数十年的事，但是人们有意识地对自己生活、生产活动的室内进行安排布置，甚至美化装饰，赋予室内环境</p>
-                            <a href='./'>阅读更多</a>
-                        </div>
-                    </li>
-                    <li className='article-item'>
-                        <div>
-                            <img src={require('../img/article-img.jpg')} alt='img'/>
-                        </div>
-                        <div className='word'>
-                            <h2>空间立体效果图,完美呈现最终效果</h2>
-                            <h5>设计文章</h5>
-                            <p>室内设计作为一门新兴的学科，尽管还只是近数十年的事，但是人们有意识地对自己生活、生产活动的室内进行安排布置，甚至美化装饰，赋予室内环境</p>
-                            <a href='./'>阅读更多</a>
-                        </div>
-                    </li>
+                    {articleList.map((item)=>(
+                        <li className='article-item' key={item.article_id}>
+                            <div>
+                                <img src={require('../img/article-img.jpg')} alt='img'/>
+                            </div>
+                            <div className='word'>
+                                <h2>{item.article_title}</h2>
+                                <h5>{item.article_time}</h5>
+                                <p>{item.article_abstract}</p>
+                                <a href='./'>阅读更多</a>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
             </div>
 
